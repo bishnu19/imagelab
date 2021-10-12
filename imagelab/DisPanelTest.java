828c94fb64c4bbc6a9c63ba560251d3198dabae1
bishnu19 <63917947+bishnu19@users.noreply.github.com>
2021-10-12 09:04:59 -0600

    Update DisPanelTest.java

--- imagelab/DisPanelTest.java
+++ imagelab/DisPanelTest.java
@@ -1,1 +1,7 @@
+package imagelab;
 
+import org.junit.Before;
+import org.junit.After;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
